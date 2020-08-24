de5f695e5e1e0ffa32c4ba39d05909111da0b132
D <802351@cognizant.com>
2020-08-24 08:43:47 +0530

    Date validation Fix

--- JS/vaidation.js
+++ JS/vaidation.js
@@ -1,4 +1,5 @@
 function updateClaimValidation() {
+    let flag = true;
     this.claimNumber = document.getElementById('ClaimNumber').value;
     this.claimPrograms = document.getElementById('ClaimPrograms').value;
     document.getElementById("ClaimNumberError").innerHTML = null;
@@ -7,32 +8,37 @@ function updateClaimValidation() {
     document.getElementById("EndDateError").innerHTML = null;
     this.EndDate = document.getElementById('EndDate').value;
     this.StartDate = document.getElementById('StartDate').value;
-    console.log("start date " + StartDate);
-    if(StartDate==null)
+    if(!this.StartDate){
+        flag=false;
         document.getElementById("StartDateError").innerHTML = "StartDate cannot be empty";
-    if(EndDate== null)
+    }
+    if(!this.EndDate){
+        flag=false;
         document.getElementById("EndDateError").innerHTML = "EndDate cannot be empty";
-    console.log("inside validation claomnumber", this.claimNumber);
-    var lettersAlphaNumeric = /^[0-9a-zA-Z]+$/;
+    }
+    let lettersAlphaNumeric = /^[0-9a-zA-Z]+$/;
     if (this.claimNumber.match(lettersAlphaNumeric)) {} else {
+        flag=false;
         document.getElementById("ClaimNumberError").innerHTML = "Claim Number should be AlphaNumeric";
     }
     if (this.claimNumber.length > 9) {
+        flag=false;
         document.getElementById("ClaimNumberError").innerHTML = "Maximum of 9 characters allowed";
     }
     if (this.claimPrograms.length > 20) {
+        flag=false;
         document.getElementById("ClaimProgramsError").innerHTML = "Maximum of 20 alphabet characters allowed";
     }
-    console.log("Date startdate "+ this.StartDate);
-    console.log("Date this.startdate "+ Date.parse(this.StartDate));
-    console.log("Date enddate "+ this.EndDate);
-    if (this.StartDate <= this.EndDate) {
-        alert("less");
+    if (Date.parse(StartDate) > Date.parse(EndDate)) {
+        flag=false;
+        document.getElementById("EndDateError").innerHTML = "End Date cannot be lesser than Start Date";
+    }
+    if(flag) {
+        location.href="view-claim-page.html";
     }
 }
 
 function cancelUpdate() {
-    console.log("inside cancel;");
     document.getElementById('ClaimNumber').value = "12345";
     document.getElementById('ClaimPrograms').value = "Accident";
     this.EndDate = document.getElementById('EndDate').value;
@@ -57,7 +63,7 @@ function passwordvalidation() {
     document.getElementById("PasswordError").innerHTML = null;
     document.getElementById("UserNameError").innerHTML = null;
     let loginflag = false;
-    var lettersAlphaNumeric = /^[0-9a-zA-Z]+$/;
+    let lettersAlphaNumeric = /^[0-9a-zA-Z]+$/;
     console.log("name: "+this.name);
     if(this.password < 4) {
         document.getElementById("PasswordError").innerHTML = "Minimum of 4 characters mandatory";
